7728b90684ce9f207bf94a1b9bb30e175bffc792
codingcele <marcocelegato9@gmail.com>
2023-02-14 16:58:49 +0100

    database projects creato e popolotato con fakers

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake() -> words(3, true),
+            'description' => fake() -> boolean() 
+                             ? fake() -> paragraph()
+                             : '',
+            'main_image' => fake() -> url(),
+            'release_date' => fake() -> date(),
+            'repo_link' => fake() -> url(),
+        ];
+    }
+}
